12f6e60b22600a468d484c8d871e4f8e14a3a932
w2019 <w2019@e-mirim.hs.kr>
2022-06-02 16:10:37 +0900

    [220602] Context API와 useContext 훅 사용

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 // import './jsxstudy/jsx-study1';
+
 // import './component-basic/first-component'
 // import './component-basic/props-basic'
 // import './component-basic/props-children'
 // import './component-basic/sol_slot_machine'
+
 // import './component-intermediate/sol_user_profile'
 // import './component-intermediate/state-study-1'
 // import './component-intermediate/state-study-with-inner-object'
 // import './component-intermediate/counter-demo'
 // import './component-intermediate/manipulate-list'
 // import './component-intermediate/todo-app-demo'
+
 // import './use-effect-demo/use-effect-intro'
 // import './use-effect-demo/use-effect-deps'
 // import './use-effect-demo/use-effect-unmount'
@@ -18,9 +21,18 @@
 // import './use-effect-demo/clickgame'
 // import './use-effect-demo/github-fetch-repos'
 // import './use-effect-demo/github-search-fetch-repos'
+
 // import './component-advanced/ref-demo'
 // import './component-advanced/controlled-component'
 // import './component-advanced/reservation-form'
 // import './component-advanced/uncontrolled-component'
+
 // import './use-ref-demo/use-ref-practice-form'
-import './use-ref-demo/use-ref-practice-form-2'
+// import './use-ref-demo/use-ref-practice-form-2'
+
+// import './component-advanced/contexts/UserContext'
+// import './component-advanced/context-api-theme-context'
+// import './component-advanced/use-context-using-user-context'
+
+// import './component-advanced/fake-login-demo'
+import './component-advanced/fake-login-button-intergrated'
